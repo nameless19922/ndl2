175845c5402e520097b9efebd8c78f220500688e
nameless19922 <melikhov.dm@gmail.com>
2017-03-23 00:34:34 +0700

    edit messages, pokemons.json

--- index.js
+++ index.js
@@ -31,22 +31,22 @@ const
                     console.log(`EXCEPTION\n${err.message}`);
                 }
             } else {
-                console.log('hide: bad params!');
+                console.log('hide bad params: "node index hide <folder> <json file>"');
             }
         },
 
         seek: args => {
             let folder = args[1];
 
-            folder ? hidenseek.seek(folder) : console.log('seek: bad params!');
+            folder ? hidenseek.seek(folder) : console.log('seek bad params: "node index seek <folder>"');
         }
     };
 
 
 if (args.length) {
-    app.hasOwnProperty(args[0]) ? app[args[0]](args) : console.log('bad params!');
+    app.hasOwnProperty(args[0]) ? app[args[0]](args) : console.log('bad params: hide or seek');
 } else {
-    console.log('node index - help');
-    console.log('node index hide ./field/ ./pokemons.json');
-    console.log('node index seek ./field/');
+    console.log('"node index" - help');
+    console.log('"node index hide <folder> <json file>" - hide pokemons from json file');
+    console.log('"node index seek <folder>" - find pokemons in the folder');
 }
